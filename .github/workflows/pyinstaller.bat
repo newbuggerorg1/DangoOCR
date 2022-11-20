471cda07a6cf03bd3abbc605d306ef2920a8fa92
NewBugger <41441631+NewBugger@users.noreply.github.com>
2022-11-20 21:18:47 +0800

    Update pyinstaller.bat

--- .github/workflows/pyinstaller.bat
+++ .github/workflows/pyinstaller.bat
@@ -1,5 +1,5 @@
-python.exe -m pip install pyinstaller
-pyinstaller --onefile --distpath pyinstaller_out --name dango_ocr --hidden-import paddle app.py
+python.exe -m pip install pyinstaller && python.exe -m pip install paddleocr && python.exe -m pip install paddlepaddle
+pyinstaller --onefile --distpath pyinstaller_out --name dango_ocr --hidden-import 'paddle' app.py
 
 dir .\pyinstaller_out\
 .\pyinstaller_out\dango_ocr.exe
