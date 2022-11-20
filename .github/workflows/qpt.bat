db6c26a3380a881202b05353324768ac15b5e88c
NewBugger <41441631+NewBugger@users.noreply.github.com>
2022-11-20 22:26:58 +0800

    Update qpt.bat

--- .github/workflows/qpt.bat
+++ .github/workflows/qpt.bat
@@ -1,7 +1,6 @@
 python.exe -m pip install --upgrade pip
 python.exe -m pip install qpt
 :: python.exe -m pip install paddleocr paddlepaddle
-python.exe -m pip install .
 
 chcp 65001
 qpt.exe -f . -p app.py -r auto -s qpt_out -h false
