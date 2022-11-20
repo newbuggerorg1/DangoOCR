95e63d4e03ee4f6cd86fd0c98c989a667ef68256
NewBugger <41441631+NewBugger@users.noreply.github.com>
2022-11-20 21:11:02 +0800

    Update pyinstaller.bat

--- .github/workflows/pyinstaller.bat
+++ .github/workflows/pyinstaller.bat
@@ -1,4 +1,5 @@
 python.exe -m pip install pyinstaller
-pyinstaller --onefile --distpath pyinstaller_out --name dango_ocr app.py
+pyinstaller --onefile --distpath pyinstaller_out --name dango_ocr --hidden-import paddle app.py
 
 dir .\pyinstaller_out\
+.\pyinstaller_out\dango_ocr.exe
