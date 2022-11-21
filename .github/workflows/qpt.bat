9381239edcf42e53bb9fda1517e8ecda0ea0d3c1
NewBugger <41441631+NewBugger@users.noreply.github.com>
2022-11-21 10:59:54 +0800

    Update qpt.bat

--- .github/workflows/qpt.bat
+++ .github/workflows/qpt.bat
@@ -1,6 +1,7 @@
 pip install qpt
 pip install pipreqs && pipreqs .\ --encoding=utf8
 :: pip install paddleocr paddlepaddle
+pip install lanms==1.0.2
 
 chcp 65001
 python.exe .github\workflows\qpt_make.py
