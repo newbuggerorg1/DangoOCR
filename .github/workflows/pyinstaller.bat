b639b6d3d8d6d80976cfe5f3b3ca5e81c8bbdf0d
NewBugger <41441631+NewBugger@users.noreply.github.com>
2022-11-21 18:03:02 +0800

    Update pyinstaller.bat

--- .github/workflows/pyinstaller.bat
+++ .github/workflows/pyinstaller.bat
@@ -1,2 +1,7 @@
+pip install pipreqs && pipreqs . --encoding=utf8 && pip install -r requirements.txt
 pip install pyinstaller
+
 pyinstaller --distpath pyinstaller_out json_loads.py
+
+dir . pyinstaller_out
+type requirements.txt
