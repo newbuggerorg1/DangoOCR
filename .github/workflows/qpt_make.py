31308a698d53a542a5edb3a9ffcf2701f0eb1828
NewBugger <41441631+NewBugger@users.noreply.github.com>
2022-11-21 10:37:54 +0800

    Update qpt_make.py

--- .github/workflows/qpt_make.py
+++ .github/workflows/qpt_make.py
@@ -5,7 +5,7 @@ module = CEM(
 	work_dir="./",
 	launcher_py_path="./app.py",
 	save_path="./qpt_out",
-	requirements_file="auto",
+	requirements_file="./requirements.txt",
 	hidden_terminal=False
 )
 
