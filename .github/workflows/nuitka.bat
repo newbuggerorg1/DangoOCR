21b2b38ccdda00ea6b3469775a4e5de4e0b60f33
NewBugger <41441631+NewBugger@users.noreply.github.com>
2022-11-21 14:35:07 +0800

    Update nuitka.bat

--- .github/workflows/nuitka.bat
+++ .github/workflows/nuitka.bat
@@ -2,8 +2,10 @@ pip install pipreqs && pipreqs . --encoding=utf8
 pip install paddlepaddle paddleocr
 pip install nuitka
 
-echo Yes | nuitka --follow-imports --include-package=paddle --standalone app.py
+echo Yes | nuitka --follow-imports --standalone app.py
 
 dir .
 dir app.build app.dist app.onefile-build
 type requirements.txt
+
+app.dist\app.exe
