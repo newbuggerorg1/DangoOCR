c9bb9dadefd299935abc429cdf14558f0fbf1cea
NewBugger <41441631+NewBugger@users.noreply.github.com>
2022-11-21 10:23:47 +0800

    Update qpt.bat

--- .github/workflows/qpt.bat
+++ .github/workflows/qpt.bat
@@ -1,6 +1,8 @@
 python.exe -m pip install --upgrade pip
 python.exe -m pip install qpt
+
 :: python.exe -m pip install paddleocr paddlepaddle
+python.exe -m pip install pipreqs && python.exe -m pipreqs .\ --encoding=utf8
 
 chcp 65001
 python.exe make.py
