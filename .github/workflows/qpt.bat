9ee0ee5d1e729ae8bb37a54a6cd0e874d9ae0003
NewBugger <41441631+NewBugger@users.noreply.github.com>
2022-11-21 16:47:45 +0800

    Update qpt.bat

--- .github/workflows/qpt.bat
+++ .github/workflows/qpt.bat
@@ -1,11 +1,9 @@
 pip install qpt
 pip install pipreqs && pipreqs .\ --encoding=utf8 && pip install -r requirements.txt
-:: pip install pytest lanms-neo paddlepaddle paddleocr
 
 chcp 65001
 python.exe .github\workflows\qpt_make.py
 
-dir .\
-dir qpt_out\
-type .\requirements.txt
-type .\requirements_with_opt.txt
+dir json_loads json_loads\qpt_out
+type json_loads\requirements.txt
+type json_loads\requirements_with_opt.txt
