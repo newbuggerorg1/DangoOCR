7ec73c741037d84aad05f0897ec77a503aa83d1f
NewBugger <41441631+NewBugger@users.noreply.github.com>
2022-11-21 17:45:17 +0800

    Update nuitka.bat

--- .github/workflows/nuitka.bat
+++ .github/workflows/nuitka.bat
@@ -1,7 +1,7 @@
 pip install pipreqs && pipreqs . --encoding=utf8 && pip install -r requirements.txt
 pip install nuitka
 
-echo Yes | nuitka --follow-imports --include-package=paddle --standalone json_loads.py
+echo Yes | nuitka --follow-imports --standalone json_loads.py
 
 dir . json_loads.build json_loads.dist json_loads.onefile-build
 type requirements.txt
