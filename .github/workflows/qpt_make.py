20f609544191566ae87a88be4d1af02cb288a61a
NewBugger <41441631+NewBugger@users.noreply.github.com>
2022-11-21 16:46:26 +0800

    Update qpt_make.py

--- .github/workflows/qpt_make.py
+++ .github/workflows/qpt_make.py
@@ -2,9 +2,9 @@
 from qpt.executor import CreateExecutableModule as CEM
 
 module = CEM(
-	work_dir="./",
-	launcher_py_path="./app.py",
-	save_path="./qpt_out",
+	work_dir="./json_loads",
+	launcher_py_path="./json_loads/app.py",
+	save_path="./json_loads/qpt_out",
 	requirements_file="auto",
 	# requirements_file="./requirements.txt",
 	hidden_terminal=False
